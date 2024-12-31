f6882bd87432e77334feacea689d8195353f2257
Anil Kul <42135771+anilkul98@users.noreply.github.com>
2024-12-31 01:38:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: '/tarim-hikayeleri/',
+  base: '/tarim-hikayeleri/',
   server: {
     host: "::",
     port: 8080,
